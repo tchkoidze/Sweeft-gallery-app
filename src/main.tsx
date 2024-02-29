a8579fef68d84de0e6a86a7d2121316cf9ae48ac
tchkoidze <ilia.chkoidze12@gmail.com>
2024-02-29 17:36:28 +0400

    wip:install axios and make api request from main page to get popoular 20 images

--- src/main.tsx
+++ src/main.tsx
@@ -8,11 +8,11 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </BrowserRouter>
 );
+
+// <React.StrictMode> </React.StrictMode>
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,50 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const accessKey = "8odbcWMjbrbIQNaomRBZSmIdlFgUFFKO6EECa_M7s9I";
+const apiUrl = "https://api.unsplash.com";
+
+interface Photo {
+  id: string;
+  downloads:number;
+  likes:number;
+  views:number;
+  urls: {
+    full:string;
+    raw:string;
+    regular: string;
+    small:string;
+    thumb:string;
+  };
+  alt_description: string;
+  width: number;
+  height: number;
+}
+
+const headers = {
+  Authorization: `Client-ID ${accessKey}`,
+};
+
 function MainPage() {
+  const photos = useQuery({
+    queryKey: ["popular_photos"],
+    queryFn: () =>
+      //axios
+        .get(`${apiUrl}/photos?page=1&per_page=20&order_by=popular`, {
+          headers: { Authorization: `Client-ID ${accessKey}` },
+        })
+        .then((res) => res.data),
+  });
+
+  if (photos.isPending) return "Loading...";
+  if (photos.error) return "An error has occurred: " + photos.error.message;
+
   return (
     <div>
       <h1>ეს არის მთავარი გვერდი</h1>
+      {photos.data.map((photo: Photo) => (
+        <img  key={photo.id} src={photo.urls.regular} alt={photo.alt_description} />
+      ))}
     </div>
   );
 }
