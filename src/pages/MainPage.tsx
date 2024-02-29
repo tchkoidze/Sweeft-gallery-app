bcef029a8a6df3f7e580f5f2b518beb9b6c40a74
tchkoidze <ilia.chkoidze12@gmail.com>
2024-02-29 21:55:19 +0400

    feat:create useDebouce custom hook

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { useState } from "react";
 
 const accessKey = "8odbcWMjbrbIQNaomRBZSmIdlFgUFFKO6EECa_M7s9I";
 const apiUrl = "https://api.unsplash.com";
@@ -26,14 +27,25 @@ const headers = {
 };
 
 function MainPage() {
+  const [searchTerm, setSearchTerm] = useState("");
+
   const photos = useQuery({
-    queryKey: ["popular_photos"],
-    queryFn: () =>
-      axios
-        .get(`${apiUrl}/photos?page=1&per_page=20&order_by=popular`, {
-          headers: { Authorization: `Client-ID ${accessKey}` },
-        })
-        .then((res) => res.data),
+    queryKey: ["photos", searchTerm],
+    queryFn: () => {
+      if (searchTerm.trim() === "") {
+        return axios
+          .get(`${apiUrl}/photos?page=1&per_page=20&order_by=popular`, {
+            headers: { Authorization: `Client-ID ${accessKey}` },
+          })
+          .then((res) => res.data);
+      } else {
+        return axios
+          .get(
+            `https://api.unsplash.com/photos/?client_id=YOUR_ACCESS_KEY&per_page=20&query=${searchTerm}`
+          )
+          .then((res) => res.data);
+      }
+    },
   });
 
   if (photos.isPending) return "Loading...";
@@ -42,6 +54,12 @@ function MainPage() {
   return (
     <div>
       <h1>ეს არის მთავარი გვერდი</h1>
+      <input
+        type="text"
+        placeholder="Search..."
+        value={searchTerm}
+        onChange={handleSearchChange}
+      />
       {photos.data.map((photo: Photo) => (
         <img
           key={photo.id}
